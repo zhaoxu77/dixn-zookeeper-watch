2ee66055b1cca106fea9626422eb7916e7863f78
zhaoxu <zhaoxu841020@163.com>
2019-06-11 14:44:26 +0800

    提交netty和线程代码

--- src/main/java/com/dixn/zookeeper/DixnWatchStarterApplication.java
+++ src/main/java/com/dixn/zookeeper/DixnWatchStarterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DixnWatchStarterApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DixnWatchStarterApplication.class, args);
     }
 
